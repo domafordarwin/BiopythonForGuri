3a2593f182544c5c776de4d7967f72ee2089a5ef
domafordarwin <domafordarwin@gmail.com>
2021-07-30 22:39:33 +0900

    Upload my code and text

---
+++ covid_19.py
@@ -0,0 +1,18 @@
+from Bio import Entrez
+
+def fetch_record(genbank_id):
+    Entrez.email = "domafordarwin@gmail.com"
+    handle = Entrez.efetch(db="nucleotide", id=genbank_id, \
+                           rettype="gb", retmode="xml")
+    records = Entrez.read(handle)
+    return records
+
+def get_sequence(records):
+    return records[0]["GBSeq_sequence"]
+
+genbank_id = "MN908947"
+records = fetch_record(genbank_id)
+sequence = get_sequence(records)
+
+print(len(sequence))
+print(sequence)
